f41749fa9d749dd8710df3cb25269f7a2eeaf710
rajipp <91722813+rajifPy@users.noreply.github.com>
2025-12-15 19:37:47 +0700

    Create photos.js

---
+++ src/data/photos.js
@@ -0,0 +1,58 @@
+export const photos = [
+  {
+    id: 1,
+    url: '/images/photos/work1.jpg',
+    caption: 'Working on data analysis project',
+    category: 'Work'
+  },
+  {
+    id: 2,
+    url: '/images/photos/work2.jpg',
+    caption: 'Team collaboration session',
+    category: 'Work'
+  },
+  {
+    id: 3,
+    url: '/images/photos/event1.jpg',
+    caption: 'Presenting at tech conference',
+    category: 'Events'
+  },
+  {
+    id: 4,
+    url: '/images/photos/event2.jpg',
+    caption: 'Bangkit Academy graduation',
+    category: 'Events'
+  },
+  {
+    id: 5,
+    url: '/images/photos/life1.jpg',
+    caption: 'Coffee and code',
+    category: 'Lifestyle'
+  },
+  {
+    id: 6,
+    url: '/images/photos/life2.jpg',
+    caption: 'Office workspace setup',
+    category: 'Lifestyle'
+  },
+  {
+    id: 7,
+    url: '/images/photos/edu1.jpg',
+    caption: 'University campus',
+    category: 'Education'
+  },
+  {
+    id: 8,
+    url: '/images/photos/edu2.jpg',
+    caption: 'Study session with friends',
+    category: 'Education'
+  }
+]
+
+export const photoCategories = [
+  'All',
+  'Work',
+  'Events',
+  'Lifestyle',
+  'Education'
+]
