25e7826ee0688ffc074b215727a4874df88dcea4
rajipp <91722813+rajifPy@users.noreply.github.com>
2026-03-16 02:45:23 +0700

    Update photos.js

--- src/data/photos.js
+++ src/data/photos.js
@@ -33,7 +33,7 @@ export const photos = [
   },
   {
     id: 6,
-    url: '/images/IMG_9682.HEIC',
+    url: '/images/DSC_5054.JPG',
     caption: 'Pelayanan Sosial Universitas Airlangga',
     category: 'Events'
   },
